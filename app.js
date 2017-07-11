a005f74dd35b0cc88e1da72075d4b3b0a45ffd3b
hdthan <than.huynh@axonactive.com>
2017-07-11 09:20:55 +0700

    create project structure

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-/*jslint esversion: 6*/
-/*jslint node: true */
-"use strict";
 let express = require('express');
 let path = require('path');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+let cors = require('cors');
+let config = require('./config/database');
+let passport = require('passport');
 
-
-let hostname = '127.0.0.2';
-let port = 3000;
+let hostname = '127.0.0.1';
+let port = process.env.PORT || 8080;
 
 let app = express(); //init app
-mongoose.connect('mongodb://localhost/test');
+let users = require('./routes/users');
+mongoose.connect(config.database);
 let db = mongoose.connection;
 
 //Check Conn
 db.once('open', function () {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB at '+ config.database);
 });
 
 //Check DB
@@ -25,7 +25,7 @@ db.on('error', function (err) {
 });
 
 //Bring in models
-let Article = require('./models/article');
+// let Article = require('./models/article');
 
 // let noti = (req, res, next) => {
 //     console.log("this is noti");
@@ -33,16 +33,21 @@ let Article = require('./models/article');
 // };
 // app.use(noti); //Middleware
 
-    let than = [{
-        age: 21,
-        lastName: 'Huynh'
-    }, {
-        age: 21,
-        lastName: 'Huynh'
-    }, {
-        age: 21,
-        lastName: 'Huynh'
-    }];
+    // let than = [{
+    //     age: 21,
+    //     lastName: 'Huynh'
+    // }, {
+    //     age: 21,
+    //     lastName: 'Huynh'
+    // }, {
+    //     age: 21,
+    //     lastName: 'Huynh'
+    // }];
+app.use(cors());
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('../eventomorrow-nodejs/config/passport')(passport);
 
 //Middleware
 app.use(bodyParser.json());
@@ -51,28 +56,38 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'client/views'));
 
+app.use('/users', users);
+
 
 //set static path
-// app.use(express.static(path.join(__dirname,'client')));
-
+app.use(express.static(path.join(__dirname,'public')));
+// Index Route
 app.get('/', (req, res) => {
-  // res.send('Hello, this is response send form server');
+    res.send('Invalid Endpoint');
+});
 
-    Article.find({}, function (err, articles) {
-        if(err){
-            console.log(err);
-        } else {
-            res.render('index', {
-                title: 'Article',
-                articles: articles
-            });
-            console.log(articles);
-        }
-
-    });
-
-  // res.render('index'); //render ejs
-}); //This is route handler, file name is index, can pass data as 2nd parameter
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+//
+// app.get('/', (req, res) => {
+//   // res.send('Hello, this is response send form server');
+//
+//     // Article.find({}, function (err, articles) {
+//     //     if(err){
+//     //         console.log(err);
+//     //     } else {
+//     //         res.render('index', {
+//     //             title: 'Article',
+//     //             articles: articles
+//     //         });
+//     //         console.log(articles);
+//     //     }
+//     //
+//     // });
+//
+//   // res.render('index'); //render ejs
+// }); //This is route handler, file name is index, can pass data as 2nd parameter
 
 app.listen(port,hostname, () => {console.log("server start and listen on port 3000..");});
 
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //database: 'mongodb://abc.com:21190/meanauthapp',   //prod
+    database: 'mongodb://localhost:27017/event',    //dev
+    secret: 'hdthan'
+};
